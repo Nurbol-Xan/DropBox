a95eddc7bd75eb61a458e79be60aaa22dd3da4e8
Student astrum <student@astrum.domain>
2022-09-02 12:04:33 +0500

    lunch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { Route, Routes} from "react-router-dom";
 import { HomePage } from './pages/HomePage/HomePages';
 
 const App = () => {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle"
-
 import { Provider } from 'react-redux';
 import store from "./redux/store";
+import {BrowserRouter} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </Provider>
 );
