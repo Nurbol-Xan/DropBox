64afd376df0a11e44cd6fe72dd053991055d8a16
Student astrum <student@astrum.domain>
2022-09-03 13:58:11 +0500

    sabBar icon done

--- src/components/DashboardComponents/SubBar/SubBar.js
+++ src/components/DashboardComponents/SubBar/SubBar.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileCirclePlus, faFileUpload, faFolderPlus } from "@fortawesome/free-solid-svg-icons";
 import "../../../CSS/SubBar.css";
 
 const SubBar = () => {
@@ -5,6 +7,19 @@ const SubBar = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-white px-4 mt-2">
                 <p className="small">Root</p>
+
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item mx-2">
+                        <button className="btn btn-outline-dark">
+                        <FontAwesomeIcon icon={faFileUpload} /> &nbsp; Upload file</button>
+                    </li>
+                    <li className="nav-item mx-2">
+                        <button className="btn btn-outline-dark"><FontAwesomeIcon icon={faFileCirclePlus} />&nbsp; Create file</button>
+                    </li>
+                    <li className="nav-item mx-2">
+                        <button className="btn btn-outline-dark"><FontAwesomeIcon icon={faFolderPlus} />&nbsp; Create folder</button>
+                    </li>
+                </ul>
             </nav>
         </>
     )
